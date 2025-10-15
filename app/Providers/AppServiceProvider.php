2ea66d3ee814efa736cf52741f54b53d0efe53d0
sdldev <sidolapakdev@gmail.com>
2025-10-15 23:09:09 +0700

    Tambahkan logika untuk memaksa HTTPS di lingkungan produksi pada AppServiceProvider. Modifikasi dilakukan pada baris 20-27 untuk meningkatkan keamanan aplikasi. 🔒🌐

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,10 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::define('admin', function ($user) {
             // return $user->role === 'admin';
             return auth()->check() && auth()->user()->role === 'admin';
-
         });
     }
 }
