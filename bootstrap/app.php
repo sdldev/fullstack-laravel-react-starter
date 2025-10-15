5bdb7a18122e379c3547a0575b89f2b44375c0c0
sdldev <sidolapakdev@gmail.com>
2025-10-15 23:16:30 +0700

    Perbaiki penggunaan namespace SecurityHeaders pada file app.php. Modifikasi dilakukan pada baris 5 untuk menghapus spasi yang tidak perlu. 🔧✨

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
-use \App\Http\Middleware\SecurityHeaders;    
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
